851f62acc36ad28b24311457112b8b4146696ab5
EdilsonMarchauek <edilson@luna.ppg.br>
2022-02-24 09:22:12 -0300

    first commit

---
+++ routes/web.php
@@ -0,0 +1,112 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+Route::get('/login', function () {
+    return 'Login';
+})->name('login');
+
+Route::middleware([])->group(function () {
+    Route::prefix('admin')->group(function () {
+        Route::namespace('Admin')->group(function () {
+            Route::name('admin.')->group(function () {
+                Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+                Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+                Route::get('/produtos', 'TesteController@teste')->name('products');
+                Route::get('/', function () {
+                    return redirect()->route('admin.dashboard');
+                })->name('home');
+            });
+        });
+    });
+});
+
+/*
+Route::group([
+    'middleware' => [],
+    'prefix' => 'admin',
+    'namespace' => 'Admin',
+    'name' => 'admin.'
+], function () {
+    Route::get('/dashboard', 'TesteController@teste')->name('dashboard');
+
+    Route::get('/financeiro', 'TesteController@teste')->name('financeiro');
+
+    Route::get('/produtos', 'TesteController@teste')->name('products');
+
+    Route::get('/', function () {
+        return redirect()->route('admin.dashboard');
+    })->name('home');
+});
+*/
+
+// ACESSANDO UM ROTA NOMEADA
+Route::get('redirect3', function () {
+    return redirect()->route('url.name');
+});
+
+// route ('url.name');
+
+// ROTAS NOMEADAS
+Route::get('/name-url', function() {
+    return 'Hey hey hey';
+})->name('url.name');
+
+// ROTA PASSANDO PARA VIEW
+// Route::get('/', function () {
+//    return view('welcome');
+// });
+
+// Rota para uma view - somente se for algo muito simples, estático, sempre passar pelo controller.
+Route::view('/view', 'welcome');
+
+// REDIRECIONAMENTO - utilizando helper (funcoes) redirect
+// Route::get('redirect1', function () {
+//     return redirect('/redirect2');
+// });
+
+// REDIRECIONAMENTO DE ROTAS
+Route::redirect('/redirect1', '/redirect2');
+
+Route::get('redirect2', function () {
+    return 'Redirect 02';
+});
+
+//ROTAS COM PARAMETROS OPCIONAIS - interrogação e define um valor para a variável
+Route::get('/produtos/{idProduct?}', function ($idProduct = '') {
+    return "Produtos(s) {$idProduct}";
+});
+
+//ROTA COM PARAMETROS - Variavel precisa ser igual ao nome da flag
+Route::get('/categorias/{flag}/posts', function ($flag) {
+    return "Posts da categoria: {$flag}";
+});
+
+//ROTA COM PARAMETROS - Variavel pode ter o nome diferente da flag
+Route::get('/categorias/{flag}', function ($prm1) {
+    return "Produtos da categoria: {$prm1}";
+});
+
+//MATCH - você define quais verbos 
+Route::match(['get', 'post'], '/match', function () {
+    return 'Match';
+});
+
+//ANY - funciona com todos o verbos http get, post
+Route::any('/any', function () {
+    return 'Any';
+});
+
+//POST Cadastrar
+Route::post('/register', function () {
+    return 'Sobre a Empresa';
+});
+
+Route::get('/empresa', function () {
+    return 'Sobre a Empresa';
+});
+
+//Dentro de pasta usar o ponto
+Route::get('/contato', function () {
+    return view('site.contact');
+});
